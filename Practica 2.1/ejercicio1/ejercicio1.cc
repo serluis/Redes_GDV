61e13bf755b60841afd56a93063880d6bb1f7a36
Marcos Docampo <marcosdo@ucm.es>
2020-04-25 22:05:27 +0200

    Ejercicio 1 terminado

--- Practica 2.1/ejercicio1/ejercicio1.cc
+++ Practica 2.1/ejercicio1/ejercicio1.cc
@@ -8,40 +8,28 @@
 // Funcion principal
 int main(int argc, char**argv) {
     struct addrinfo hints;
-    struct addrinfo *res;
-    // Lista de posibles conexiones
-    struct sockaddr *sa;
+    struct addrinfo * res;
 
     memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;
 
-    for (int i = 0; i < 2; ++i) {
-        // Los dos tipos de INET
-        if (i == 0) hints.ai_family = AF_INET;
-        else hints.ai_family = AF_INET6;
+    // Coger la informacion
+    int rc = getaddrinfo(argv[1], NULL, &hints, &res);
+    // Control de errores de la informacion
+    if (rc != 0) {
+        std::cerr << "Error: " << gai_strerror(rc) << std::endl;
+        return -1;
+    }
 
-        for (int j = 0; j < 3; ++j) {
-            // Los tres tipos de SOCKET
-            switch (j) {
-                case 0: hints.ai_socktype = SOCK_STREAM; break;
-                case 1: hints.ai_socktype = SOCK_DGRAM; break;
-                case 2: hints.ai_socktype = SOCK_RAW; break;
-            }
+    for (struct addrinfo * rp = res; rp != NULL; rp = rp->ai_next) {
+        char host[NI_MAXHOST];
 
-            // Coger la informacion
-            int rc = getaddrinfo(argv[1], NULL, &hints, &res);
-            // Control de errores de la informacion
-            if (rc != 0) {
-                std::cerr << "Error: " << gai_strerror(rc) << std::endl;
-                return -1;
-            }
+        getnameinfo((struct sockaddr *)rp->ai_addr, rp->ai_addrlen, host, NI_MAXHOST, NULL, NI_MAXSERV, NI_NUMERICHOST);
 
-            char host[NI_MAXHOST];
-            getnameinfo(res->ai_addr, res->ai_addrlen, host, NI_MAXHOST, NULL, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
-
-            // Salida del programa
-            std::cout << host << "  " << res->ai_family << "    " << res->ai_socktype << std::endl;
-        }
+        // Salida del programa
+        std::cout << host << "  " << rp->ai_family << "    " << rp->ai_socktype << std::endl;
     }
     freeaddrinfo(res);
+
     return 0;
 }
