d7747e2ac5902219b878187f867497695fc96994
Marcos Docampo <marcosdo@ucm.es>
2020-04-26 15:23:46 +0200

    Ejercicio 2 terminado

--- Practica 2.1/ejercicio2/ejercicio2.cc
+++ Practica 2.1/ejercicio2/ejercicio2.cc
@@ -14,7 +14,7 @@ int main(int argc, char **argv) {
     struct addrinfo * res;
 
     // ---------------------------------------------------------------------- //
-    // INICIALIZACIÓN SOCKET & BIND //
+    // --------------------- INICIALIZACIÓN SOCKET & BIND ------------------- //
     // ---------------------------------------------------------------------- //
 
     memset(&hints, 0, sizeof(struct addrinfo));
@@ -38,7 +38,7 @@ int main(int argc, char **argv) {
     freeaddrinfo(res);
 
     // ---------------------------------------------------------------------- //
-    // RECEPCIÓN MENSAJE DE CLIENTE //
+    // ------------- RECEPCIÓN MENSAJE DE CLIENTE Y RESPUESTA --------------- //
     // ---------------------------------------------------------------------- //
     char buffer[80];
     char host[NI_MAXHOST];
@@ -49,8 +49,9 @@ int main(int argc, char **argv) {
 
     ssize_t bytes;
     do {
+        // Libera la memoria del buffer para que no se sobreescriba
         memset(&buffer, 0, sizeof(buffer));
-
+        // Control de errores del buffer
         bytes = recvfrom(sd, buffer, 79 * sizeof(char), 0, &client_addr, &client_len);
         if (bytes == -1) {
             std::cerr << "recvfrom: " << std::endl;
@@ -59,16 +60,35 @@ int main(int argc, char **argv) {
 
         // Coger la informacion
         getnameinfo(&client_addr, client_len, host, NI_MAXHOST, service, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
-        if (strlen(buffer) <= 2 * sizeof(char)) {
+
+        // Tiempo
+        time_t now;
+        struct tm * structtime;
+        time(&now);
+        structtime = localtime(&now);
+        // Declaracion del mensaje que recibe el cliente
+        char msg[sizeof(buffer)];
+        memset(&msg, 0, sizeof(msg));
+        // MENSAJES
+        if (strlen(buffer) == 2 * sizeof(char)) {
             switch (buffer[0]) {
                 case 't': {
+                    // Mensaje en el servidor
                     std::cout << bytes << " bytes de " << host << ":" << service << std::endl;
+                    // Respuesta al cliente
+                    strftime(msg, sizeof(msg), "%T%p", structtime);
+                    sendto(sd, msg, strlen(msg), 0, &client_addr, client_len);
                 } break;
                 case 'd': {
+                    // Mensaje en el servidor
                     std::cout << bytes << " bytes de " << host << ":" << service << std::endl;
+                    // Respuesta al cliente
+                    strftime(msg, sizeof(msg), "%F", structtime);
+                    sendto(sd, msg, strlen(msg), 0, &client_addr, client_len);
                 } break;
                 case 'q': {
                     std::cout << bytes << " bytes de " << host << ":" << service << std::endl;
+                    std::cout << "Saliendo..." << std::endl;
                 } break;
                 default: {
                     std::cout << bytes << " bytes de " << host << ":" << service << std::endl;
@@ -80,19 +100,7 @@ int main(int argc, char **argv) {
             std::cout << bytes << " bytes de " << host << ":" << service << std::endl;
             std::cout << "Comando no soportado " << buffer;
         }
-    } while (buffer[0] != 'q' && strlen(buffer) <= 2);
-
-    std::cout << "IP: " << host << " PUERTO: " << service << " MENSAJE: " << buffer << std::endl;
-    // ---------------------------------------------------------------------- //
-    // RESPUESTA AL CLIENTE //
-    // ---------------------------------------------------------------------- //
-    time_t now;
-    struct tm *info;
-    time(&now);
-    //info = localtime(&now);
-    //std::cout << asctime(info);
-    std::cout << now;
-    sendto(sd, buffer, bytes, 0, &client_addr, client_len);
+    } while (!(buffer[0] == 'q' && strlen(buffer) == 2));
 
     return 0;
 }
